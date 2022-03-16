4a7fc08a1d80b11dec72fd7fc97dff0c1d9b4904
andriusmw <andriusmw@yahoo.es>
2022-03-16 16:33:22 +0100

    probando variable, recibir datos

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 function App() {
 
-  
+  var nombre ="El Pato Lucas";
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola, soy {nombre}
         </p>
         <a
           className="App-link"
