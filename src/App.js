fe129d88bafbc8295e7404151e12e565c74a58db
andriusmw <andriusmw@yahoo.es>
2022-03-16 17:12:01 +0100

    empezando con los useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ReactDOM } from 'react';
 import './App.css';
+//Hay que importar useState from React
+import {useState} from "react";
+//useState se importa entre corchetes porque su libreria por defecto no es react, para importar algo en concreto de un sitio
+//que no es el suyo por defecto se mete entre corchetes. Sino no compila
 
 function App() {
 
-
+const [linea1] = useState('Hola Mundo');
+//esto se llama crear un estado, dentro del parentesis se le
+//pasa valor pro defecto a useState(). 
+//esta declarando dos constantes y les va a pasar los valores mediante el usetate.
+//el primer valor a linea1 y el segundo a setlinea1
 
   return (
     <div className="App">
@@ -31,9 +40,10 @@ function App() {
       <button>Exportar</button>
 
         <div>
-          <span>Linea 1</span>
-          <span>Linea 2</span>
-          <img src="" />
+          <span>{linea1}</span> <br />
+          <span>Linea 2</span>  <br />
+          <img src="" alt='no image'></img>
+        
         </div>
 
     </div>
